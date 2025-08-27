4f7c6703b64c0f75975bf700a12d46480aed7bba
JSDID <icegouse@gmail.com>
2025-08-27 20:23:25 +0300

    Fix: promo

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import Promo from "./components/Promo";
-import promoImg from "./components/promo-bg.jpg";
-import { useNavigate, Routes, Route } from "react-router-dom";
+import { useNavigate, Routes, Route } from 'react-router-dom';
+import Promo from './components/Promo';
+import promoImg from './components/trail-lesson.png';
+
+const promoData = {
+  title: 'Пробний урок',
+  description:
+    'Визначивши рівень, щоб оцінити методику, формат навчання, а також познайомитися з викладачем та одногрупниками, ми запрошуємо вас відвідати пробне заняття, яке не потребує оплати.',
+  buttonLabel: 'Забронювати',
+  image: promoImg,
+};
 
 function App() {
   const navigate = useNavigate();
-  const handleBuyClick = () => {
-    navigate("/buy");
-  };
+
+  const handleBuyClick = () => navigate('/buy');
+
   return (
     <Routes>
+      <Route path="/" element={<Promo {...promoData} onButtonClick={handleBuyClick} />} />
       <Route
-        path="/"
-        element={
-          <Promo
-            title="Лучший шоколад для вас!"
-            description="Побалуйте себя или близких нашим фирменным шоколадом из отборных ингредиентов."
-            buttonLabel="Купить"
-            onButtonClick={handleBuyClick}
-            image={promoImg}
-          />
-        }
+        path="/buy"
+        element={<div style={{ padding: 40, fontSize: 24 }}>Страница покупки</div>}
       />
-      <Route path="/buy" element={<div style={{padding: 40, fontSize: 24}}>Страница покупки</div>} />
     </Routes>
   );
 }
